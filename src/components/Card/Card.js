46e252f1587c49e59a52b8554ad53145c80813fc
vicocongui <vicufranco2001@gmail.com>
2023-08-10 13:32:00 -0300

    second commit

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import Style from './Card.module.css'
 
-const Card = () => {
+
+
+const Card = ({title, txt, url}) => {
   return (
-    <div>Card ssss</div>
+      <article className={Style.card_wrapper}>
+        <img className={Style.img} src={url} alt={title}/>
+        <div className= {Style.div_content}>
+        <h3 className={Style.title}>{title}</h3>
+        <p className={Style.txt}>{txt}</p>
+        <button>a</button>
+        </div>
+      </article>
   )
 }
 
--- src/components/CardsContainer/CardsContainer.js
+++ src/components/CardsContainer/CardsContainer.js
@@ -1,20 +1,48 @@
 import React from 'react'
 import Card from '../Card/Card'
+import Style from './CardsContainer.module.css'
+
+const data = [
+  {
+    title: 'Rick Sánchez',
+    url: '/assets/img/rick.jpg',
+    txt: {
+      uno:
+        'El abuelo',
+      dos:
+      '',
+    },
+  },
+  {
+    title: 'Morty Smith',
+    url: '/assets/img/morty.jpg',
+    txt: {
+      uno:
+        'El nieto',
+      dos:
+      '',
+    },
+  },
+  {
+    title: 'Mr poopybutthole',
+    url: '/assets/img/mr.jpg',
+    txt: {
+      uno:
+        'Solo Mr poopybutthole',
+      dos:
+      '',
+    },
+  },
+];
 
 const CardsContainer = () => {
+  return ( 
+  <section className={Style.card_wrapper}>
+    {data.map(({title, txt, url}) => (
+      <Card title={title} txt={txt.uno} url={url} />
+    ))}
+  </section>
+  );
+};
 
-  return (
-    <div>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </div>
-  )
-}
-
-export default CardsContainer
+export default CardsContainer;
